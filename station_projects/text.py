d974d731cce74a401532436e830a3a97328336d2
alternativefuture <yepremyanhovhannes11@gmail.com>
2023-07-13 10:09:47 +0400

    v1

---
+++ station_projects/text.py
@@ -0,0 +1,87 @@
+stories = {"1":
+    {
+        "template": """It was about {} {} ago when I arrived at the hospital in a {}. The hospital is a {} place, 
+        there are a lot of {} {} here. There are nurses here who have {} {}. If someone wants to come into my room I 
+        told them that they have to {} first. I’ve decorated my room with {} {}. Today I talked to a doctor and they 
+        were wearing a {} on their {}. I heard that all doctors {} {} every day for breakfast. The most {} thing 
+        about being in the hospital is the {} {}!""",
+        "story_quizz": (
+            "a number",
+            "measure of time",
+            "a mode of transportation",
+            "an adjective",
+            "an another adjective",
+            "a noun",
+            "a color",
+            "a part of the body",
+            "a verb",
+            "an another number",
+            "an another noun",
+            "one another noun",
+            "an another part of the body",
+            "an another verb",
+            "an another noun",
+            "an another adjective",
+            "a silly word",
+            "a noun again"
+        )
+    },
+    "2":
+        {
+            "template": """This weekend I am going camping with {}. I packed my lantern, sleeping bag, and {}. I am 
+            so {} to {} in a tent. I am {} we might see a {}, I hear they’re kind of dangerous. While we’re camping, 
+            we are going to hike, fish, and {}. I have heard that the {} lake is great for {}. Then we will {} hike 
+            through the forest for {} {}. If I see a {} {} while hiking, I am going to bring it home as a pet! At 
+            night we will tell {} {} stories and roast {} around the campfire!!""",
+            "story_quizz": (
+                "a Persons name",
+                "a noun",
+                "an adjective",
+                "a verb",
+                "an another adjective",
+                "an animal",
+                "an another verb",
+                "a color",
+                "a verb + ing",
+                "a adverb + ly",
+                "a number",
+                "measure of time",
+                "an another color",
+                "an another animal",
+                "an another number",
+                "a silly word",
+                "a noun again"
+            )
+        },
+
+    "3":
+        {
+            "template": """Dear {}, I am writing to you from a {} castle in an enchanted forest. I found myself here 
+            one day after going for a ride on a {} {} in {}. There are {} {} and {} {} here! In the {} there is a 
+            pool full of {}. I fall asleep each night on a {} of {} and dream of {} {}. It feels as though I have 
+            lived here for {} {}. I hope one day you can visit, although the only way to get here now is {} on a {} {} 
+            !!""",
+            "story_quizz": (
+                "a proper noun (Person’s name)",
+                "an adjective",
+                "a color",
+                "an animal",
+                "a place",
+                "an another adjective",
+                "a magical Creature (Plural)",
+                "an another adjective",
+                "a magical Creature (Plural) 2",
+                "a room in a house",
+                "a noun",
+                "an another noun",
+                "an noun(Plural) 3",
+                "an another adjective",
+                "an noun(Plural) 4",
+                "a number",
+                "measure of time",
+                "a verb  + ing",
+                "an adjective again",
+                "a noun again"
+            )
+        }
+}
